192008b96f13a9882e960d3113a3b20a77c98a43
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2024-09-19 11:42:23 -0300

    codigo visto no video

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css"
+import {useState} from "react"
 
 function App() {
+
+  const [num_goals_python, setNumGoalsPython] = useState(0)
+
+  function adicionarGoalPython(){
+    setNumGoalsPython(num_goals_python + 1);
+  }
 
   return (
       <div className="cabecalho">
         <div className="time">
           <img src="/Python_FC.png" alt="logo Python FC" width={200} />
-          <button className="btn_goal">Goal</button>
+          <button className="btn_goal" onClick={() => adicionarGoalPython()}>Goal</button>
         </div>
         <div className="time">
           <img src="/Javascript.png" alt="logo Javascript da Colina" width={250} />
@@ -16,7 +23,7 @@ function App() {
         <h2>Placar</h2>
         <p>
           PFC
-          <span className="num_goals">0</span>
+          <span className="num_goals">{num_goals_python}</span>
           x
           <span className="num_goals">0</span>
           JSC
